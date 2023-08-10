2906cda37692dd688bccbfafbd909a8ffc1f0323
yudododo <135632056+yudododo@users.noreply.github.com>
2023-08-10 12:15:09 +0800

    Add files via upload

---
+++ rice website/app-copy.py
@@ -0,0 +1,76 @@
+from __future__ import unicode_literals
+from flask import Flask, request, abort, render_template
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import requests
+import json
+import configparser
+import os
+from urllib import parse
+app = Flask(__name__, static_url_path='/static')
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+my_line_id = config.get('line-bot', 'my_line_id')
+end_point = config.get('line-bot', 'end_point')
+line_login_id = config.get('line-bot', 'line_login_id')
+line_login_secret = config.get('line-bot', 'line_login_secret')
+my_phone = config.get('line-bot', 'my_phone')
+HEADER = {
+    'Content-type': 'application/json',
+    'Authorization': F'Bearer {config.get("line-bot", "channel_access_token")}'
+}
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@app.route('/line_login', methods=['GET'])
+def line_login():
+    if request.method == 'GET':
+        code = request.args.get("code", None)
+        state = request.args.get("state", None)
+
+        if code and state:
+            HEADERS = {'Content-Type': 'application/x-www-form-urlencoded'}
+            url = "https://api.line.me/oauth2/v2.1/token"
+            FormData = {"grant_type": 'authorization_code', "code": code, "redirect_uri": F"{end_point}/line_login", "client_id": line_login_id, "client_secret":line_login_secret}
+            data = parse.urlencode(FormData)
+            content = requests.post(url=url, headers=HEADERS, data=data).text
+            content = json.loads(content)
+            url = "https://api.line.me/v2/profile"
+            HEADERS = {'Authorization': content["token_type"]+" "+content["access_token"]}
+            content = requests.get(url=url, headers=HEADERS).text
+            content = json.loads(content)
+            name = content["displayName"]
+            userID = content["userId"]
+            pictureURL = content["pictureUrl"]
+            statusMessage = content.get("statusMessage","no exist")
+            print(content)
+            return render_template('userex.html', name=name, pictureURL=
+                                   pictureURL, userID=userID, statusMessage=
+                                   statusMessage)
+        else:
+            return render_template('login.html', client_id=line_login_id,
+                                   end_point=end_point)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
